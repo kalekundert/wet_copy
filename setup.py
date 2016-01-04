d64c780c47a768c71fcd2c5b353ef7a58b613d4a
Kale Kundert <kale@thekunderts.net>
2016-01-04 16:26:49 -0500

    Use setuptools if possible.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-import distutils.core
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 # Uploading to PyPI
 # =================
@@ -16,29 +19,29 @@ from wet_copy import __version__, __author__, __email__
 with open('README.rst') as file:
     readme = file.read()
 
-distutils.core.setup(
-        name='wet_copy',
-        version=__version__,
-        author=__author__,
-        author_email=__email__,
-        url='https://github.com/kalekundert/wet_copy',
-        download_url='https://github.com/kalekundert/wet_copy/tarball/v'+__version__,
-        license='GPLv3',
-        description="Format and print wetlab protocols stored as text files in git repositories.",
-        long_description=readme,
-        keywords=[
-            'print',
-            'scientific',
-            'protocols',
-        ],
-        py_modules=[
-            'wet_copy',
-        ],
-        install_requires=[
-            'docopt==0.6.2',
-        ],
-        entry_points = {
-            'console_scripts': ['wet_copy=wet_copy:main'],
-        },
+setup(
+    name='wet_copy',
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    url='https://github.com/kalekundert/wet_copy',
+    download_url='https://github.com/kalekundert/wet_copy/tarball/v'+__version__,
+    license='GPLv3',
+    description="Format and print wetlab protocols stored as text files in git repositories.",
+    long_description=readme,
+    keywords=[
+        'print',
+        'scientific',
+        'protocols',
+    ],
+    py_modules=[
+        'wet_copy',
+    ],
+    install_requires=[
+        'docopt==0.6.2',
+    ],
+    entry_points = {
+        'console_scripts': ['wet_copy=wet_copy:main'],
+    },
 
 )
